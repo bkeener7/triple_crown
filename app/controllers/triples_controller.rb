aaf368de93258c77e8f33ee9b5f5bb51f1c4e186
bkeener <bkeener7@gmail.com>
2023-05-16 12:42:41 -0600

    Refactor Triples controller to use before_action

--- app/controllers/triples_controller.rb
+++ app/controllers/triples_controller.rb
@@ -1,36 +1,36 @@
 class TriplesController < ApplicationController
+  before_action :find_triple, only: [:show, :edit, :update, :destroy]
+
   def index
     @triples = Triple.order_by_created_at
   end
 
-  def show
-    @triple = Triple.find(params[:id])
-  end
+  def show; end
 
   def new; end
 
   def create
     new_triple = Triple.create!(triple_params)
-    redirect_to '/triples'
+    redirect_to triples_path
   end
 
-  def edit
-    @triple = Triple.find(params[:id])
-  end
+  def edit; end
 
   def update
-    triple = Triple.find(params[:id])
-    triple.update(triple_params)
-    redirect_to "/triples/#{triple.id}"
+    @triple.update(triple_params)
+    redirect_to "/triples/#{@triple.id}"
   end
 
   def destroy
-    triple = Triple.find(params[:id])
-    triple.destroy
-    redirect_to('/triples')
+    @triple.destroy
+    redirect_to triples_path
   end
 
   private
+
+  def find_triple
+    @triple = Triple.find(params[:id])
+  end
 
   def triple_params
     params.permit(:country, :year_established, :dirt_track)
